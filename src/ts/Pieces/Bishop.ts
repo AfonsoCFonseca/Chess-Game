9274688ad1d56b1b479586e7ed1e3e46f452acf4
Afonso Fonseca <afonseca@visor.ai>
2021-08-07 11:55:53 +0100

    Bishop Class done

--- src/ts/Pieces/Bishop.ts
+++ src/ts/Pieces/Bishop.ts
@@ -1,6 +1,9 @@
 import { PiecesColors, PiecesType, TilePositionInterface } from '../game.interfaces';
 import Piece from './Piece';
 import * as utils from '../Utils/utils';
+import Tile from '../Board/Tile';
+import { board } from '../App';
+import { BOARD_SIZE } from '../Utils/consts';
 
 export default class Bishop extends Piece {
     constructor(tilePos: TilePositionInterface, color: PiecesColors) {
@@ -8,4 +11,40 @@ export default class Bishop extends Piece {
         const imageName = `${color === PiecesColors.WHITE ? 'white' : 'black'}Bishop`;
         super({ positionX, positionY }, color, PiecesType.BISHOP, imageName);
     }
+
+    public showPossibleMoves(): Tile[] {
+        const possibleLeft = this.getMoves('left');
+        const possibleRight = this.getMoves('right');
+        const finalPossibleTiles = possibleLeft.concat(possibleRight);
+        return super.showPossibleMoves(finalPossibleTiles);
+    }
+
+    private getMoves(type: 'left' | 'right'): Tile[] {
+        const possibleArrMoves = [];
+        const direction = ['down', 'up'];
+
+        for (let i = 0; i < direction.length; i++) {
+            const currentTile = { 
+                tileX: this.currentTile.tileX + (type === 'left' ? -1 : 1),
+                tileY: this.currentTile.tileY + (direction[i] === 'up' ? -1 : 1)
+            };
+
+            let piece = board.getPieceOnTile(currentTile);
+            while ((!piece || piece.color !== this.color) && isInsideBoard(currentTile)) {
+                const tile = board.getTiles(currentTile);
+                if (!piece || (piece && piece.color !== this.color)) possibleArrMoves.push(tile);
+                if (piece) break;
+
+                currentTile.tileX += (type === 'left' ? -1 : 1);
+                currentTile.tileY += (direction[i] === 'up' ? -1 : 1);
+                piece = board.getPieceOnTile(currentTile);
+            }
+        }
+        return possibleArrMoves;
+    }
+}
+
+function isInsideBoard(currentTile): boolean {
+    return (currentTile.tileX < BOARD_SIZE && currentTile.tileX >= 0) 
+        && (currentTile.tileY < BOARD_SIZE && currentTile.tileY >= 0);
 }
